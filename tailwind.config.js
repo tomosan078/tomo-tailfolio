e8956ccc29c964fce1d74ea1682b21071031ec74
tomosan078 <119410187+tomosan078@users.noreply.github.com>
2026-02-10 06:28:27 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        floatY: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-50px)" }
+        },
+        floatY28: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-50px)" }
+        }
+      },
+      animation: {
+        floatY: "floatY 20s ease-in-out infinite",
+        floatY28: "floatY28 28s ease-in-out infinite"
+      }
+    }
+  },
+  plugins: []
+};
